7e64c16e6b77d4c3f1ab4b0273a91ae1c4b3a57f
Sam Todd <stodd03@calpoy.edu>
2021-01-19 13:09:58 -0800

    updated documentation

--- bears_tests.py
+++ bears_tests.py
@@ -3,15 +3,19 @@ from bears import *
 
 class TestAssign1(unittest.TestCase):
     def test_bear_01(self):
+        # test for bears requiring multiple steps
         self.assertTrue(bears(250))
 
     def test_bear_02(self):
+        # test for starting with correct # of bears
         self.assertTrue(bears(42))
 
     def test_bear_03(self):
+        # test for false requiring steps
         self.assertFalse(bears(53))
 
     def test_bear_04(self):
+        # test for false with too few bears
         self.assertFalse(bears(41))
 
 if __name__ == "__main__":
