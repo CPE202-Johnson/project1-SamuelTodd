e45e175463bec19ee78ba22406203fbbd02f556d
Sam Todd <stodd03@calpoy.edu>
2021-01-19 00:19:28 -0800

    does not progress to next condition?

--- bears.py
+++ bears.py
@@ -1,1 +1,34 @@
+# int -> boolean
+# returns a boolean value representing the possibility of winning the game with n bears
 def bears(n):
+    # base case 1
+    if n == 42:
+        return True
+    
+    # base case 2
+    if n < 42:
+        return False
+    
+    # even
+    if n%2 == 0:
+        give = n/2
+        if bears(n - give) == True:
+            return True
+
+    # divisible by 3 or 4
+    if (n%3 == 0) or (n%4 == 0):
+        lastd = n%10
+        secondlastd = (n%100)/10
+        give = lastd * secondlastd
+        if give > 0:
+            if bears(n - give) == True:
+                return True
+    
+    # divisible by 5
+    if n%5 == 0:
+        give = 42
+        if bears(n - give) == True:
+            return True
+
+    # not possible
+    return False
