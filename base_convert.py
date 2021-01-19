e58f26528611c157f5f883b159371159d278fb83
Sam Todd <stodd03@calpoy.edu>
2021-01-18 20:32:37 -0800

    completed function

--- base_convert.py
+++ base_convert.py
@@ -1,3 +1,19 @@
-
+# int int -> str
+# returns a string representing num in the base b
 def convert(num, b):
-    """Recursive function that returns a string representing num in the base b"""
+    # finds the quotient of the num and the base
+    quotient = num//b
+
+    # stops if quotient is 0
+    if quotient == 0:
+        return num
+
+    # assigns remainder
+    else:
+        remainder = num%b
+
+        # assigns the letter symbols if the remainder >= 10
+        if remainder >= 10:
+            symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            remainder = symbols[remainder-10]
+        return str(convert(quotient, b)) + str(remainder)
