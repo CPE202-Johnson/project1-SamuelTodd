adbab56cb3573dd90b3483a562f80b0062dcdf6c
Sam Todd <stodd03@calpoy.edu>
2021-01-18 20:00:08 -0800

    added test cases

--- perm_lex_testcases.py
+++ perm_lex_testcases.py
@@ -4,7 +4,17 @@ import perm_lex
 class TestAssign1(unittest.TestCase):
 
     def test_perm_gen_lex(self):
-        self.assertEqual(perm_lex.perm_gen_lex('ab'),['ab','ba'])
+        # test for an empty string
+        self.assertEqual(perm_lex.perm_gen_lex(''), [])
+
+        # test for a single character string
+        self.assertEqual(perm_lex.perm_gen_lex('a'),['a'])
+
+        # default test for a two character string
+        self.assertEqual(perm_lex.perm_gen_lex('ab'), ['ab','ba'])
+
+        # test for a three letter string
+        self.assertEqual(perm_lex.perm_gen_lex('abc'), ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
 
 if __name__ == "__main__":
         unittest.main()
